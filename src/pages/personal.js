321043a090554c3bd4f20aceb9eb8a92d64a23a0
Alec Divito <divito.alec@gmail.com>
2019-08-04 11:26:01 -0400

    renamed projects page to personal page

--- src/pages/personal.js
+++ src/pages/personal.js
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const ProjectsPage = () => (
+const PersonalsPage = () => (
     <Layout>
         <SEO title="About Alec" />
         <h1>Hi, I'm Alec Di Vito :)</h1>
@@ -11,4 +11,4 @@ const ProjectsPage = () => (
     </Layout>
 );
 
-export default ProjectsPage;
+export default PersonalsPage;
