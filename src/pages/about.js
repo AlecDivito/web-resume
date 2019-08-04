4147eaa0eae1c70bdbde167bd09013dbb47b1e04
Alec Divito <divito.alec@gmail.com>
2019-08-04 00:49:04 -0400

    Got started on the index page + added sweet profile pic

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,36 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const AboutPage = () => (
-  <Layout>
-    <SEO title="You... wanna know more about... me :)" />
-    <h1>So I try my best and I guess this is my best.</h1>
-    <p>About my you say. I go to school and bike. That sums up my life.</p>
-  </Layout>
-)
+const AboutPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(
+        relativePath: { eq: "Profile_Pic_Alec_Divito.png" }
+      ) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <div>
+        <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+      </div>
+      <SEO title="You... wanna know more about... me :)" />
+      <h1>So I try my best and I guess this is my best.</h1>
+      <p>About my you say. I go to school and bike. That sums up my life.</p>
+    </Layout>
+  );
+}
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,30 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "./index.scss";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="home">
+        <header className="home__header">
+          <h1 className="home__header--name">Alec Di Vito</h1>
+          <h3 className="home__header--title">Full Stack Web Developer</h3>
+        </header>
+        <section className="home__section">
+          <p className="home__section__description">
+            Hi there! I produce elegant, performance and accessible digital
+            experiences
+          </p>
+          <div className="home__header__links">
+            <Link to="/projects/">explore projects</Link>
+            <Link to="/about/">view profile</Link>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
