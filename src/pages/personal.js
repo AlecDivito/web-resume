42a35fdece3302ed1dea560dc54442c39723159a
Alec Divito <divito.alec@gmail.com>
2019-08-07 19:36:53 -0400

    made personal page desktop ready

--- src/pages/personal.js
+++ src/pages/personal.js
@@ -5,6 +5,7 @@ import { useStaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
 
 import githubIcon from "../images/github-square.svg";
+import linkIcon from "../images/link.svg";
 import "./personal.scss";
 import Header from "../components/header";
 
@@ -19,6 +20,7 @@ const PersonalsPage = () => {
                         stage
                         technologies
                         githubLink
+                        siteLink
                     }
                 }
             }
@@ -63,17 +65,13 @@ const PersonalsPage = () => {
     return (
         <Layout>
             <SEO title="Projects" />
-            <div className="personal">
+            <div className="personal layout--max-width">
                 <Header text="Featured Personal Projects" isCenter={true} />
-                <p>These are the projects that I'm most proud of and like to show off!</p>
-                <div>
+                <p className="personal__description">These are the projects that I'm most proud of and like to show off!</p>
+                <div className="personal__cards">
                 {data.site.siteMetadata.personalProjects.map((t, i) =>
                     <div className="card" key={i} >
-                        <Img
-                            css={{ top: 0, left: 0, right: 0, bottom: 0 }}
-                            style={{ position: `absolute`, borderRadius: "10px", filter: `blur(1px) brightness(30%)`, width: `100%`, height: `100%`}}
-                            fluid={data[t.picId].childImageSharp.fluid}
-                        />
+                        <Img className="card__image" fluid={data[t.picId].childImageSharp.fluid} />
                         <div className="card__content">
                             <div className="card__header">
                                 <h3>{t.title}</h3>
@@ -83,19 +81,27 @@ const PersonalsPage = () => {
                                 <div className="card__footer__tags">
                                     {t.technologies.map((t, i) => <span className="tag" key={i} >{t}</span>)}
                                 </div>
-                                {(t.githubLink)
-                                    ? <a href={t.githubLink} target="_blank" rel="noopener noreferrer">
-                                        <img className="card__footer__link" alt="Github Url for title" src={githubIcon} />
-                                    </a>
-                                    : null
-                                }
+                                <div className="card__footer__links">
+                                    {(t.siteLink)
+                                        ? <a href={t.siteLink} target="_blank" rel="noopener noreferrer">
+                                            <img className="card__footer__links__link" alt="Site Url for title" src={linkIcon} />
+                                        </a>
+                                        : null
+                                    }
+                                    {(t.githubLink)
+                                        ? <a href={t.githubLink} target="_blank" rel="noopener noreferrer">
+                                            <img className="card__footer__links__link" alt="Github Url for title" src={githubIcon} />
+                                        </a>
+                                        : null
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
                 )}
                 </div>
                 <Header text="Other Personal Project" isCenter={true} />
-                <p>
+                <p className="personal__description">
                     These are projects I worked on when I was younger and less experienced
                     or just weren't able to fully finish
                 </p>
