86eff4675d53303aa85cc184777a2f4a3079652d
Alec Divito <divito.alec@gmail.com>
2019-08-06 23:21:47 -0400

    created snow flake yay looks :fire:

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,110 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "./index.scss";
 
+/**
+ * Render a storm of particles that are really pretty
+ */
+class Particles {
+
+  /**
+   * Create a class to handle particle updating and rendering
+   * 
+   * @param {HTMLCanvasElement} canvas to render
+   * @param {int} maxParticles number of particles to render
+   */
+  constructor(canvas, maxParticles) {
+    if (!canvas) {
+      throw new Error('Couldn\'t create particles because canvas doesn\'t exist');
+    }
+    this.canvas = canvas;
+    this.context = canvas.getContext('2d');
+    this.particles = [];
+    this.maxParticles = maxParticles;
+    this.width = canvas.clientWidth;
+    this.height = canvas.clientHeight;
+    canvas.height = this.height;
+    canvas.width = this.width;
+    this.angle = 0;
+    this.deltaTime = 1 / 60;
+
+    for (let i = 0; i < maxParticles; i++) {
+      this.particles.push({
+        x: Math.random() * this.width,
+        y: Math.random() * this.height,
+        radius: Math.random() * 5 + 1,
+        density: Math.random() * maxParticles,
+        color: "rgba(" + Math.floor((Math.random() * 255)) + ", " + Math.floor((Math.random() * 255)) + ", " + Math.floor((Math.random() * 255)) + ", 0.8)",
+      });
+    }
+
+    // on resize, resize height and width
+    window.addEventListener('resize', () => {
+      this.width = canvas.clientWidth;
+      this.header = canvas.clientHeight;
+      canvas.height = this.height;
+      canvas.width = this.width;
+    });
+  }
+
+  /**
+   * Update the particles in the canvas
+   */
+  update() {
+    this.angle += this.deltaTime / 3;
+    for (let i = 0; i < this.maxParticles; i++) {
+      let particle = this.particles[i];
+      // update coordinates
+      // density calculates how much the angle influences the flake
+      particle.y += (Math.cos(this.angle + particle.density / 50) + 1 + particle.radius / 2);
+      particle.x += Math.sin(this.angle) * 2;
+
+      // Send particles to opposite sides of the screen if threshold is crossed
+      if (particle.x > this.width + particle.radius
+         || particle.x < -1 * particle.radius
+         || particle.y > this.height + particle.radius)
+      {
+        particle.x = Math.random() * this.width;
+        particle.y = -10;
+      }
+    }
+  }
+
+  /**
+   * Render the particles on the canvas
+   */
+  render() {
+    this.context.clearRect(0, 0, this.width, this.height);
+    for (let i = 0; i < this.maxParticles; i++) {
+      let particle = this.particles[i];
+      this.context.beginPath();
+      this.context.fillStyle = particle.color;
+      this.context.moveTo(particle.x, particle.y);
+      this.context.arc(particle.x, particle.y, particle.radius, 0, 2 * Math.PI);
+      this.context.fill();
+    }
+  }
+
+}
+
 const IndexPage = () => {
 
-  const update = () => {
+  let particles = null;
 
+  const update = () => {
+    // get context
+    if (!particles) {
+      let canvas = document.getElementById('bg-canvas');
+      if (canvas) {
+        particles = new Particles(canvas, 25);
+        console.log(particles);
+      } else {
+        requestAnimationFrame(update);
+        return;
+      }
+    }
+    particles.update();
+    particles.render();
+    requestAnimationFrame(update);
   }
 
   requestAnimationFrame(update);
@@ -16,7 +116,7 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <canvas className="home--background"></canvas>
+      <canvas id="bg-canvas" className="home--background"></canvas>
       <div className="home">
         <div></div>
         <header className="home__header">
@@ -25,14 +125,14 @@ const IndexPage = () => {
           </h1>
           <h3 className="home__header--title">Full Stack Web Developer</h3>
           <p className="home__section__description">
-            Hi there! I produce elegant, performance and accessible digital
-            experiences
+            Aspiring Designer and Machine Learning Engineer
           </p>
+          <Link to="/about">Learn More</Link>
         </header>
         <section className="home__section">
           <div className="home__section__links">
-            <Link className="home__section__links--link link" to="/personal/">explore projects</Link>
-            <Link className="home__section__links--link link" to="/about/">view profile</Link>
+            <Link className="home__section__links--link link" to="/personal/">projects</Link>
+            <Link className="home__section__links--link link" to="/about/">work</Link>
           </div>
         </section>
       </div>
