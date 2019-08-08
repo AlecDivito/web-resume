bfb223121d713170c4073d89c803a8d8db44a1b3
Alec Divito <divito.alec@gmail.com>
2019-08-07 21:58:28 -0400

    Added data to update site with standard practices

--- gatsby-config.js
+++ gatsby-config.js
@@ -179,29 +179,41 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    {
+    { // for the images
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    /*
+    {
+      resolve: 'gatsby-source-filesystem
+      options: {
+        path: './data/
+      }
+
+      // .data/ has json
+      // ./data/images has images
+    }
+    */
+    `gatsby-transformer-sharp`, // images
+    `gatsby-plugin-sharp`,      // image optimization
+    `gatsby-transformer-json`,  // graphql for data files
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Alec Di Vito`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
