844799d607d352a897d6fed38915beac4b1dee5e
Alec Divito <divitoa@Alecs-MacBook-Pro.local>
2019-09-02 00:58:52 -0400

    updated header into sticky header

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,35 +5,37 @@ import Logo from "./logo"
 
 const Nav = ({ links, navActive, onNavClick, children }) => (
   <nav className="nav">
-    <div className="nav--header">
-        <Link to="/" className="nav__logo">
-          <Logo />
-        </Link>
+    <div className="nav--border-shadow layout--max-width">
+      <div className="nav--header">
+          <Link to="/" className="nav__logo">
+            <Logo />
+          </Link>
 
-        <div className="burger--wrapper">
-        <div
-            className={`burger ${navActive ? "burger--active" : ""}`}
-            onClick={onNavClick}
-        />
+          <div className="burger--wrapper">
+          <div
+              className={`burger ${navActive ? "burger--active" : ""}`}
+              onClick={onNavClick}
+          />
+          </div>
+      </div>
+
+      <div className={`nav__mobile ${navActive ? "nav__list--dropdown" : ""}`}>
+        <div className="nav__list">
+          {(links) ? links.filter(item => item.ready).map((item, index) => (
+          <Link
+              key={item.id}
+              to={item.link}
+              className="nav__list__item"
+              activeClassName="nav__list__item--active"
+          >
+              {item.title}
+          </Link>
+          )) : null}
+        </div>
+        <div className="nav__children">
+          {children}
         </div>
     </div>
-
-    <div className={`nav__mobile ${navActive ? "nav__list--dropdown" : ""}`}>
-      <div className="nav__list">
-        {(links) ? links.filter(item => item.ready).map((item, index) => (
-        <Link
-            key={item.id}
-            to={item.link}
-            className="nav__list__item"
-            activeClassName="nav__list__item--active"
-        >
-            {item.title}
-        </Link>
-        )) : null}
-      </div>
-      <div className="nav__children">
-        {children}
-      </div>
     </div>
   </nav>
 )
