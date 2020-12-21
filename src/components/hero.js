01ec9aa48b68277579e5b99f0b3930f7c8ab1fdf
Alec Di Vito <divito.alec@gmail.com>
2020-12-21 09:05:18 -0500

    Completed hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,25 +3,33 @@ import PropTypes from "prop-types"
 import "./hero.scss";
 import { Link } from "gatsby";
 import Tag from "./tag";
+import HeroCurve from "../assets/hero.svg";
 
-const Hero = ({ title, titleIcon, description, tags, readMoreLink, date, children }) => (
+const Hero = ({ title, titleIcon, subTitle, tags, readMoreLink, readMoreText, date, children }) => (
     <div className="hero">
-        {children}
-        <div className="hero--flex">
-            <h2 className="hero__title">
-                {titleIcon}
-                {title}
-            </h2>
-            <p className="hero__description">{description}</p>
-            <div>
-                <div>
-                    <ul className="hero__list">
-                        {tags.map((t) => <li className="hero__list__item"><Tag text={t} /></li>)}
-                    </ul>
-                    {(readMoreLink) ? <Link to={readMoreLink}>Continue Reading</Link> : null}
-                </div>
-                {(date) ? <p>Last Updated: {date}</p> : null}
+        <div className="hero--container layout--max-width">
+            <div className="hero--z-index" style={{ width: 220 }}>
+                {children}
             </div>
+            <div className="hero--z-index hero--content">
+                <h2 className="hero__title">
+                    {titleIcon}
+                    {title}
+                </h2>
+                <p className="hero__description">{subTitle}</p>
+                <div>
+                    <div className="hero--flex">
+                        <ul className="hero__list">
+                            {tags.map((t) => <li className="hero__list__item"><Tag text={t} /></li>)}
+                        </ul>
+                        {(readMoreLink && readMoreText) ? <Link className="hero__link" to={readMoreLink}>{readMoreText}</Link> : null}
+                    </div>
+                    {(date) ? <p>Last Updated: {date}</p> : null}
+                </div>
+            </div>
+        </div>
+        <div className="hero__curve">
+            <HeroCurve />
         </div>
     </div>
 );
@@ -29,10 +37,11 @@ const Hero = ({ title, titleIcon, description, tags, readMoreLink, date, childre
 Hero.prototype = {
     title: PropTypes.string.isRequired,
     titleIcon: PropTypes.node,
-    description: PropTypes.string.isRequired,
-    tags: PropTypes.arrayOf(PropTypes.string),
+    subTitle: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
     readMoreLink: PropTypes.string,
-    date: PropTypes.string,
+    readMoreText: PropTypes.string,
+    date: PropTypes.string.isRequired,
     children: PropTypes.node
 }
 
