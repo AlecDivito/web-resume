6e8f08835c93cac6b7609a05113caaf3d1961007
Alec Di Vito <divito.alec@gmail.com>
2020-12-23 13:03:34 +0200

    updated tag component

--- src/components/tag.js
+++ src/components/tag.js
@@ -2,12 +2,16 @@ import React from "react";
 import PropTypes from 'prop-types';
 import "./tag.scss"
 
-const Tag = ({ text }) => (
-    <span className="tag">{text}</span>
+const Tag = ({ text, dropshadow }) => (
+    <span className={`tag ${dropshadow ? "tag--shadow" : ""}`}>
+        {text}
+    </span>
 )
 
+
 Tag.propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string.isRequired,
+    dropshadow: PropTypes.bool,
 }
 
 export default Tag;
