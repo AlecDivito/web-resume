4e5ad1144813ad47b2340344e7f2cb8da3efb845
Alec Di Vito <divito.alec@gmail.com>
2020-12-23 15:33:21 +0200

    added complex statistic component

---
+++ src/components/complex/statistic.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { VerticalList } from "../simple/list";
+import Title from '../simple/title';
+import "./statstic.scss"
+
+const Statistic = ({ image, bigImage, title, subTitle, link }) => (
+    <VerticalList className="statistic">
+        {image ? <img className={`statistic__img ${bigImage ? "statistic__img--big" : ""}`} src={image} alt={title} /> : null}
+        {title ? <Title variant="h5" className="statistic__title">
+            {link ? <a className="statistic__title" href={link}>{title}</a> : title}
+        </Title> : null
+        }
+        <p>{subTitle}</p>
+    </VerticalList>
+)
+
+Statistic.prototype = {
+    image: PropTypes.string,
+    bigImage: PropTypes.bool,
+    title: PropTypes.string,
+    subTitle: PropTypes.string.isRequired,
+    link: PropTypes.string,
+}
+
+export default Statistic;
