ec8bb45bc79e78018049c83a2bba76acf8869525
Alec Di Vito <divito.alec@gmail.com>
2020-12-23 15:34:05 +0200

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,84 +1,91 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Image from "../components/image";
-import ProfilePicture from "../components/profilePicture";
+import Hero from "../components/hero";
+import Box from "../components/simple/box";
+import { HorizontalList } from "../components/simple/list";
+import Title from "../components/simple/title";
+import Widget from "../components/widget";
 import "./about.scss";
-import Header from "../components/header";
+import Statistic from "../components/complex/statistic";
 
 const query = graphql`
 query AboutMeQuery {
-  allAboutJson {
-    nodes {
-      id
-      details
-      link
-      section
+  dataJson {
+    about {
+      info {
+        title
+        items {
+          bigImage
+          image
+          link
+          subTitle
+          title
+        }
+      }
+      widgets {
+        description
+        title
+      }
+    }
+  }
+  file( relativePath: { eq: "images/about/Profile_Pic_Alec_Divito.png" } ) {
+    childImageSharp {
+      fixed(width: 220, height: 220) {
+        ...GatsbyImageSharpFixed
+      }
     }
   }
 }
 `;
 
-const AboutPage = () => { 
+const AboutPage = () => {
   const data = useStaticQuery(query)
   return (
     <Layout>
       <SEO title="About Me" />
-      <section className="about layout--max-width">
-        <div className="about__greetings">
-          <ProfilePicture />
-          <div className="about__greetings__intro">
-            <Header text="Nice to meet you" isCenter={true} />
-            <p className="about__intro">
-              My name is <strong>Alec Di Vito</strong>. I'm a creative, ambitious, and enterprising software engineer.
-            </p>
-            <p className="about__intro">
-              Currently I'm looking for co-op opportunities for the 2020 Winter
-              and Summer semesters. If you like what you see and want to work with me
-              <strong><Link className="link link--default-color" to="/contact"> please get in touch.</Link></strong>
-            </p>
-          </div>
+      <Hero
+        title="About Alec"
+        subTitle="Professional and Personal"
+        tags={["Programmer", "Wannabe Pro Cyclist", "Expert Canoist"]}
+      >
+        <Img style={{ borderRadius: '100%' }} fixed={data.file.childImageSharp.fixed} alt="Alec Divito Profile Picture" />
+      </Hero>
+      <div className="common common--max-width">
+        <div className="common__content common--content--max-width">
+          <Title varient="h2" className="">About Me</Title>
+          <section>
+            {data.dataJson.about.widgets.map(w =>
+              <Widget title={w.title} description={w.description} key={w.title} />
+            )}
+          </section>
+          {data.dataJson.about.info.map(w =>
+            <section>
+              <Title varient="h3">{w.title}</Title>
+              <HorizontalList>
+                {w.items.map(item =>
+                  <Statistic {...item} />
+                )}
+              </HorizontalList>
+            </section>
+          )}
+          <section>
+            <Title varient="h2">And there is more coming soon!</Title>
+          </section>
         </div>
-        {data.allAboutJson.nodes.map( ({id, details, link, section}) => (
-          <div key={id} className="about__me">
-            <h3 className="about__me__header">{section}</h3>
-            <ul className="about__me__details">
-              {details.map( (description, i) =>
-                <li key={i} className="about__me__details__item">
-                  {description}
-                </li>
-              )}
+        <div className="common--box">
+          <Box className="common--box--sticky">
+            <ul>
+              <li>This is it's own component. Will impl later</li>
+              <li>Content</li>
+              <li>Content</li>
+              <li>Content</li>
             </ul>
-            <div className="about__me__link">
-              {(link) ?
-                <Link to={link} className="about__me__link link link--default-color">
-                  Learn More
-                </Link>
-                : null
-              }
-            </div>
-          </div>
-        ))}
-        <div className="about__greetings">
-          <div className="about__greetings__intro">
-            <Header text="That's not all I do" isCenter={true} />
-            <p className="about__intro">
-              I also really enjoy riding my bike and helping out
-              around the community. During the past summer, I helped out a bike shop
-              in Mississauga that is run parallel between Sheridan College and the City of
-              Mississauga.
-            </p>
-            <p className="about__intro">
-              I love skating and was Head Skate Patrol at multiple different
-              rinks around Mississauga growing up. I really love the GTA and can't wait to
-              do more.
-            </p>
-          </div>
-          <Image />
+          </Box>
         </div>
-      </section>
+      </div>
     </Layout>
   );
 }
