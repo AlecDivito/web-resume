76e30237b393910d067230fb19475b8951919109
Alec Di Vito <divito.alec@gmail.com>
2021-01-08 22:16:22 +0200

    updated food and hero svg for resize

--- src/components/statusDot.js
+++ src/components/statusDot.js
@@ -2,15 +2,15 @@ import React from "react";
 import PropTypes from "prop-types"
 import "./statusDot.scss"
 
-const StatusDot = ({ status }) => (
-    <div className="status--container">
+const StatusDot = ({ status, className }) => (
+    <div className={`status--container ${className ?? ""}`}>
         <div className={`status--ringring ${status}`}></div>
         <div className={`status--circle ${status}`}></div>
     </div>
 )
 
 StatusDot.propTypes = {
-    status: PropTypes.oneOf(['completed', 'active', 'archived'])
+    status: PropTypes.oneOf(['completed', 'active', 'archived']).isRequired
 }
 
 export default StatusDot;
